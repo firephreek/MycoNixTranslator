7405576c65fe8e2d36da8175d23ec9c3b3b87906
Vladimir Donich <git@funlennysub.dev>
2025-07-13 14:24:24 +0300

    Remove useless upgrade loading

--- Translator/Core.cs
+++ Translator/Core.cs
@@ -27,17 +27,6 @@ class UpgradePath
 
 public class Core : MelonMod
 {
-    private bool _hasLoaded;
-
-    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-    {
-        if (_hasLoaded) return;
-        var type = Il2CppInterop.Runtime.Il2CppType.Of<Il2Cpp.Upgrade>();
-        var upgrades = UnityEngine.Resources.FindObjectsOfTypeAll(type);
-        LoggerInstance.Msg($"Loaded {upgrades.Count} upgrades");
-        _hasLoaded = upgrades.Count > 0;
-    }
-
     public override void OnInitializeMelon()
     {
         LoggerInstance.Msg($"Initialized!");
